ef2dc3e10db74ef5af184171b4f978a66bfa1c1c
Rayman3007 <fizullin.ayaz@gmail.com>
2023-05-21 16:21:38 +0300

    change data handling

--- data_handling.py
+++ data_handling.py
@@ -19,7 +19,7 @@ def morph_analyzer(text):
 
 #оставляем только слова
 def remove_special_characters(text, remove_digits=True):
-    pattern=r'[^\u0401\u0451\u0410-\u044fa-zA-z0-9\s]'
+    pattern=r'[^\u0401\u0451\u0410-\u044f\s]'
     text=re.sub(pattern,'',text)
     return text
 
@@ -35,19 +35,22 @@ def remove_stopwords(text, is_lower_case=False):
         filtered_tokens = [token for token in tokens if token not in stopword_list]
     else:
         filtered_tokens = [token for token in tokens if token.lower() not in stopword_list]
-    filtered_text = ' '.join(filtered_tokens)    
+    filtered_text = ' '.join(filtered_tokens)
     return filtered_text
 
-data_reviews = pd.read_parquet(r"C:\Users\Admin\Downloads\train.parquet")
-data_reviews['text']=data_reviews['text'].apply(remove_stopwords)
-data_reviews['text']=data_reviews['text'].apply(morph_analyzer)
+data_reviews = pd.read_parquet(r"train.parquet")
 data_reviews['text']=data_reviews['text'].apply(remove_special_characters)
+data_reviews['text']=data_reviews['text'].apply(morph_analyzer)
+data_reviews['text']=data_reviews['text'].apply(remove_stopwords)
+
 norm_train_reviews = data_reviews
 norm_train_reviews.to_parquet('norm_train_reviews.parquet', engine='fastparquet')
 
-data_test_reviews = pd.read_parquet(r"C:\Users\Admin\Downloads\test.parquet")
-data_test_reviews['text']=data_test_reviews['text'].apply(remove_stopwords)
-data_test_reviews['text']=data_test_reviews['text'].apply(morph_analyzer)
+data_test_reviews = pd.read_parquet(r"test.parquet")
 data_test_reviews['text']=data_test_reviews['text'].apply(remove_special_characters)
+data_test_reviews['text']=data_test_reviews['text'].apply(morph_analyzer)
+data_test_reviews['text']=data_test_reviews['text'].apply(remove_stopwords)
+
 norm_test_reviews = data_test_reviews
 norm_test_reviews.to_parquet('norm_test_reviews.parquet', engine='fastparquet')
+
